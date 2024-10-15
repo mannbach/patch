65b0bc9dcc48363fce2d30e9ec0a48f23c6fe19e
Jan Bachmann <jan@mannbach.de>
2024-10-15 19:43:35 +0000

    Update validator to both LFM args

--- patch/model_config.py
+++ patch/model_config.py
@@ -1,28 +1,52 @@
 """Defines a dataclass to store the configuration of the PATCH model and validate its configuration.
 """
+from typing import Union
 from dataclasses import dataclass
 
 from netin.models import CompoundLFM
 
-class _LFMTValidator:
-    """Descriptor to validate the value of the local link formation mechanism.
+class _LFMValidator:
+    """Descriptor to validate the value of the link formation mechanisms.
     """
     _name: str
 
     def __set_name__(self, owner, name):
         self._name = "_" + name
 
-    def __get__(self, obj, type):
+    def __get__(self, obj: "ModelConfig", _):
         return getattr(obj, self._name)
 
-    def __set__(self, obj, value):
+    def __set__(self, obj: "ModelConfig", value: Union[str, CompoundLFM]):
+        """Sets the LFm values.
+        If the value is a string, it is converted to a CompoundLFM.
+        If the LFM is for triadic closure edges, it must be either
+        UNIFORM or identical to the global LFM.
+
+        Parameters
+        ----------
+        obj : ModelConfig
+            The configuration object.
+        value : Union[str, CompoundLFM]
+            The value to set.
+
+        Raises
+        ------
+        ValueError
+            If the value is not a CompoundLFM.
+        ValueError
+            If the LFM is for triadic closure edges and
+            the value is not UNIFORM or identical to the global LFM.
+        """
+        if isinstance(value, str):
+            value = CompoundLFM[value]
         if not isinstance(value, CompoundLFM):
             raise ValueError(f"Expected a CompoundLFM, got {type(value)}")
-        if not value in (CompoundLFM.UNIFORM, obj.lfm_global):
+        if (self._name == "_lfm_tc") and (not value in (CompoundLFM.UNIFORM, obj.lfm_global)):
             raise ValueError(
-                (f"Expected a CompoundLFM different from {obj.lfm_global} "
-                 f"or {CompoundLFM.UNIFORM}, got {value}"))
-        setattr(obj, self._name, int(value))
+                ("CompoundLFM for triadic closure edges must be "
+                 f"{CompoundLFM.UNIFORM} or identical to the global LFM "
+                 f"({obj.lfm_global.value}); got {value}."))
+        setattr(obj, self._name, value)
 
 @dataclass
 class ModelConfig:
@@ -33,6 +57,6 @@ class ModelConfig:
     minority_fraction: float
     homophily: float
     tau: float
-    lfm_global: CompoundLFM
-    lfm_tc: _LFMTValidator = _LFMTValidator() # Validate the local link formation mechanism
+    lfm_global: _LFMValidator = _LFMValidator()
+    lfm_tc: _LFMValidator = _LFMValidator() # Validate the local link formation mechanism
     realization: int
