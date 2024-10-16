2f49ec902b52a1bc3a53c543364c98e15ca78b52
Jan Bachmann <jan@mannbach.de>
2024-10-16 15:12:03 +0000

    Add lineplots

--- patch/constants.py
+++ patch/constants.py
@@ -1,4 +1,5 @@
 from netin.models import CompoundLFM
+import matplotlib.pyplot as plt
 
 # Default values
 N = 5000
@@ -12,7 +13,10 @@ L_LFM_LOCAL = [lfm.value for lfm in CompoundLFM]
 L_HOMOPHILY = [0.01, 0.25, 0.5, 0.75, 0.99]
 L_TAU = [0., 0.2, 0.5, 0.8, 1.]
 
+# IO
 PATH_GRAPHS = "./data/graphs/"
 PATH_STATISTICS = "./output/aggregate_statistics.csv"
+PATH_PLOTS = "./output/plots/"
 
+# Computation
 STOP_SIGNAL = -1
