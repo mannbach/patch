dee0ec5b3ef9c916a43db3f5e0b3d1098352b746
Jan Bachmann <bachmann@csh.ac.at>
2025-02-04 18:10:21 +0000

    Add folder for validatoin

--- patch/constants.py
+++ patch/constants.py
@@ -18,6 +18,7 @@ PATH_STATISTICS = "./output/stats/"
 PATH_PLOTS = "./output/plots/"
 PATH_APS = "./data/empirical/"
 PATH_INFERENCE = "./output/inference/"
+PATH_INFERENCE_VALIDATION = "./output/inference/validation/"
 
 # Computation
 STOP_SIGNAL = -1
