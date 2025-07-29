1fa6a27cf6deba7e552e215421d9360d7a0ccd1b
Jan Bachmann <bachmann@csh.ac.at>
2025-07-29 15:27:06 +0000

    Test computation of m values

---
+++ tests/test_m.py
@@ -0,0 +1,21 @@
+from netin.models import PATCHModel, CompoundLFM
+
+from patch.elfi import compute_m
+
+def test_compute_m():
+    for n in [10, 100, 1000]:
+        for m in [2, 4, 8]:
+            model = PATCHModel(
+                f_m=.2,
+                N=n,
+                m=m,
+                tau=0.,
+                lfm_global=CompoundLFM.UNIFORM,
+                lfm_tc=CompoundLFM.UNIFORM,
+            )
+            graph = model.simulate()
+
+            m_comp = compute_m(graph_empirical=graph)
+            assert m_comp == m, (
+                f"Expected m={m}, but got {m_comp} for n={n}, m={m}."
+            )
